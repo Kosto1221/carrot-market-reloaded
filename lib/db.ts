ac7aa8a84e257883e1eddc069f82907727696630
Kosto1221 <ostolany@gamil.com>
2024-11-25 21:08:20 +0900

    onDelete

--- lib/db.ts
+++ lib/db.ts
@@ -2,18 +2,18 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.findUnique({
-    where: {
-      id: 1,
-    },
-    include: {
-      user: true,
-    },
-  });
-  console.log(token);
-}
-test();
+// async function test() {
+//   const token = await db.sMSToken.findUnique({
+//     where: {
+//       id: 1,
+//     },
+//     include: {
+//       user: true,
+//     },
+//   });
+//   console.log(token);
+// }
+// test();
 
 // async function test() {
 //   const token = await db.sMSToken.create({
