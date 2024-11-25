de7afb40a33f670f5ae9615ca3c0c142f3962da4
Kosto1221 <ostolany@gamil.com>
2024-11-25 19:45:47 +0900

    Relationships

--- lib/db.ts
+++ lib/db.ts
@@ -2,6 +2,34 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
+async function test() {
+  const token = await db.sMSToken.findUnique({
+    where: {
+      id: 1,
+    },
+    include: {
+      user: true,
+    },
+  });
+  console.log(token);
+}
+test();
+
+// async function test() {
+//   const token = await db.sMSToken.create({
+//     data: {
+//       token: "12121212",
+//       user: {
+//         connect: {
+//           id: 1,
+//         },
+//       },
+//     },
+//   });
+//   console.log(token);
+// }
+// test();
+
 // async function test() {
 //   const user = await db.user.create({
 //     data: {
